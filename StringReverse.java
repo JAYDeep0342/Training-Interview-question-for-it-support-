5714cef067b01a9982d738fc0f077c913be28018
Jaydeep0342 <jaydeep0342@gmail.com>
2026-06-04 16:13:20 +0530

    Array program

---
+++ StringReverse.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class StringReverse{
+    public static void main(String[] args){
+        String str ="Hello";
+        String rev ="";
+        for(int i = str.length()-1; i>=0;i--){
+       rev = rev + str.charAt(i);
+        }
+        System.out.println(rev);
+    }
+}
